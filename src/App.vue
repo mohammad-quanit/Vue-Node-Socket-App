2c709f202bd5499fee20e6955461c92b9bb05771
Mohammad-Quanit <muhammadquanit@gmail.com>
2019-11-25 10:53:11 +0500

    vue js socket implementation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Chat />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Chat from "./components/Chat";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    // HelloWorld,
+    Chat
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import socketio from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
 
 Vue.config.productionTip = false
+
+export const SocketInstance = socketio('http://localhost:4001');
+// Vue.use(VueSocketIO, SocketInstance);
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketInstance,
+  // options: { path: "/my-app/" } //Optional options
+}));
 
 new Vue({
   render: h => h(App),
